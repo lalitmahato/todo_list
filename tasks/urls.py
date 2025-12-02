defa2e2d62d4277f281011defb1047923e74e33b
lalitmahato <lalitmahato11@gmail.com>
2025-12-03 03:27:31 +0545

    update and delete operations

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,8 +3,10 @@ tasks urls
 """
 from django.contrib import admin
 from django.urls import path
-from tasks.views import task_list
+from tasks.views import task_list, update_task, delete_task
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path('edit/<int:id>/', update_task, name='update_task'),
+    path('delete/<int:id>/', delete_task, name='delete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -18,3 +18,24 @@ def task_list(request):
     }
     return render(request, "index.html", context)
 
+
+def update_task(request, id):
+    instance = Task.objects.get(id=id)
+    tasks = Task.objects.all()
+    form = TaskForm(request.POST or None, instance=instance)
+    if request.method == "POST":
+        if form.is_valid:
+            form.save()
+            return redirect('task_list')
+    context = {
+        'tasks': tasks,
+        'form': form
+    }
+    return render(request, "index.html", context)
+
+
+def delete_task(request, id):
+    instance = Task.objects.get(id=id)
+    instance.delete()
+    return redirect('task_list')
+
